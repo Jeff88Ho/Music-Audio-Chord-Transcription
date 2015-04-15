04e3da8b1436861d3989b56f2a7965c5753c7685
Jeff Ho <wh2318@columbia.edu>
2015-04-15 00:10:19 -0400

    Create data_1.py

---
+++ data_1.py
@@ -0,0 +1,56 @@
+__author__ = 'Jeff'
+
+
+from scipy.io import loadmat
+import os
+
+dir = "/Users/Jeff/Desktop/svm_hmm/CHORDS"
+
+num = 0
+
+for file in os.listdir(dir):
+    num+=1
+
+    file_name = os.path.join(file)
+    print file_name
+
+
+    read_path = os.path.join(dir, file)
+    print read_path
+
+
+    mat = loadmat(read_path)
+
+
+    F = mat['F']
+    L = mat['L']
+
+
+
+    F = F.transpose()
+
+
+
+    print F.shape
+    print L.shape
+
+    L = L[range(len(L)),0]
+
+
+    write_path = "/Users/Jeff/Desktop/svm_hmm/input/" + str(file_name) + ".txt"
+
+    text_file = open(write_path, "w")
+
+
+    for i in range(int(F.shape[0])):
+        text_file.write(str(L[i]+1)+" "+"qid:"+str(num)) #all label values plus 1
+
+        for j in range(int(F.shape[1])):
+            text_file.write(" "+str(j+1)+":"+str(F[i, j]))
+        text_file.write(str("\n"))
+
+
+
+    text_file.close()
+
+
